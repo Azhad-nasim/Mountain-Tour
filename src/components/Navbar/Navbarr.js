29c06363588496843207a7b24d84102007b0fa5b
AZHAD <azhadnasim.786@gmail.com>
2023-06-21 00:35:51 +0530

    rendering issues solved agained-1

--- src/components/Navbar/Navbarr.js
+++ src/components/Navbar/Navbarr.js
@@ -14,10 +14,13 @@ const Nav1 = () => {
     setClick((current) => !current);
   };
   function openNewTab() {
-    window.open("https://azhad-nasim.github.io/Mountain-Tour", "_blank");
+    window.open(
+      "https://azhad-nasim.github.io/Mountain-Tour/history",
+      "_blank"
+    );
   }
   function openNewTab2() {
-    window.open("https://azhad-nasim.github.io/Mountain-Tour", "_blank");
+    window.open("https://azhad-nasim.github.io/Mountain-Tour/team", "_blank");
   }
 
   return (
