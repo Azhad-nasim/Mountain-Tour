b06e60bf1c2d3577832670f54baa6a9270b14dd1
AZHAD <azhadnasim.786@gmail.com>
2023-06-20 17:27:15 +0530

    rendering issues solved

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
       <BrowserRouter basename="/Mountain-Tour">
         <Routes>
           <Route exact path="/Mountain-Tour" element={<Nav1 />} />
-          <Route path="/Mountain-Tour/history" element={<History />}></Route>
-          <Route path="/Mountain-Tour/team" element={<Team />}></Route>
+          <Route path="/history" element={<History />}></Route>
+          <Route path="/team" element={<Team />}></Route>
         </Routes>
       </BrowserRouter>
     </>
